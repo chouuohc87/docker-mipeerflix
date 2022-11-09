af50146fb213b922d331d69098d982fc7f81f1f7
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 15:59:29 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,9 +21,4 @@ iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
 iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
 iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner root -j REDSOCKS
 sleep 10
-echo "curl with socks"
-curl -x socks5://127.0.0.1:1080 ifconfig.me
-echo "curl without socks"
-curl ifconfig.me
 node index.js
-sleep 10
