c91a6bd0534fe3ae315ca9ab24aba9c9c7888067
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 15:51:16 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ sleep 10
 echo "Restarting redsocks and redirecting traffic via iptables"
 redsocks -v -c /etc/redsocks.conf
 iptables -t nat -N REDSOCKS
+iptables -t nat -A REDSOCKS -d 85ptc4-5000.sse.codesandbox.io -j RETURN
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
 iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
 iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner root -j REDSOCKS
