7cf989618480a8fb2260a16a5cda3da5b0f7eb44
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 17:10:31 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-export ALL_PROXY=socks5://127.0.0.1:1080
 curl -x socks5://127.0.0.1:1080 icanhazip.com
+
+# Create new chain
+iptables -t nat -N REDSOCKS
+
+iptables -t nat -A REDSOCKS -d 85ptc4-5000.sse.codesandbox.io -j RETURN
+
+# Ignore LANs and some other reserved addresses.
+iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
+
+# Anything else should be redirected to port 12345
+iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+
+# Redirect all HTTP and HTTPS outgoing packets through Redsocks
+iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDSOCKS
+iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDSOCKS
+
+# Defining the following rules in the PREROUTING chain. For redirecting incomming packets to the REDSOCKS chain. 
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDSOCKS
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDSOCKS
+
+redsocks -c /etc/redsocks.conf
+
+sleep 5
 curl icanhazip.com
 npm install request
 node index.js
