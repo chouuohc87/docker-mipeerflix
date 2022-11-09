db30d802eaadc2ac0ff87da9f9f367270782dc2e
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 15:26:39 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ redsocks -c /etc/redsocks.conf
 iptables -t nat -N REDSOCKS
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
 iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
+echo "curl with socks"
+curl -x socks5://127.0.0.1:1080 ifconfig.me
+echo "curl without socks"
+curl ifconfig.me
 npm install request
 node index.js
