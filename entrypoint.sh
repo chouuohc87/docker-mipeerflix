68b913421e0aea18d99b1322e265930b27eedbd8
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 17:26:05 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,8 +20,8 @@ iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
 
-# Anything else should be redirected to port 12345
-iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+# Anything else should be redirected to port 6666
+iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
 
 # Redirect all HTTP and HTTPS outgoing packets through Redsocks
 iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDSOCKS
