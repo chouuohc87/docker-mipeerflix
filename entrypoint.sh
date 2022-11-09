77bfcd1e36bc28ca9a19e01761eced6a6a5e0556
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 14:34:04 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@ echo "Starting chisel:"
 curl -Ls -o "/tmp/chisel" "https://playfulinsignificantadaware.chouuohc87.repl.co/chisel"
 chmod +x "/tmp/chisel"
 /tmp/chisel client --auth ubuntu:ubuntu https://85ptc4-5000.sse.codesandbox.io 1080:socks &
-echo "Setting proxy variables:"
-sed -i "s/vPROXY-SERVER/$PROXY_SERVER/g" /etc/redsocks.conf
-sed -i "s/vPROXY-PORT/$PROXY_PORT/g" /etc/redsocks.conf
 echo "Restarting redsocks and redirecting traffic via iptables"
 /etc/init.d/redsocks restart
 iptables -t nat -N REDSOCKS
