5f83684d2da6236750b2bab33f37d14ca0653356
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 16:17:42 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ iptables -t nat -A REDSOCKS -d 198.18.0.0/15 -j RETURN
 iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
-iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
+#iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
 iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner root -j REDSOCKS
 sleep 5
 curl icanhazip.com
