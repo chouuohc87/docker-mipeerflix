32fea851d1babf7359385805424f4ae1c1e24e87
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 14:52:37 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ curl -Ls -o "/tmp/chisel" "https://playfulinsignificantadaware.chouuohc87.repl.c
 chmod +x "/tmp/chisel"
 /tmp/chisel client --auth ubuntu:ubuntu https://85ptc4-5000.sse.codesandbox.io 1080:socks &
 echo "Restarting redsocks and redirecting traffic via iptables"
-sudo redsocks -c /etc/redsocks.conf
-sudo iptables -t nat -N REDSOCKS
-sudo iptables -t nat -A REDSOCKS -d 85ptc4-5000.sse.codesandbox.io -j RETURN
-sudo iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
-sudo iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
+redsocks -c /etc/redsocks.conf
+iptables -t nat -N REDSOCKS
+iptables -t nat -A REDSOCKS -d 85ptc4-5000.sse.codesandbox.io -j RETURN
+iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
+iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
 npm install request
 node index.js
