a66278fd223b5443f4b742df9b13dcb3a91ce943
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 15:37:07 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Starting chisel"
 chisel client --auth ubuntu:ubuntu https://85ptc4-5000.sse.codesandbox.io 1080:socks &
-sleep 30
+sleep 10
 echo "Restarting redsocks and redirecting traffic via iptables"
+redsocks -h
+cat /etc/redsocks.conf
 redsocks -c /etc/redsocks.conf
 iptables -t nat -N REDSOCKS
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
@@ -13,3 +15,4 @@ echo "curl without socks"
 curl ifconfig.me
 npm install request
 node index.js
+sleep 30
