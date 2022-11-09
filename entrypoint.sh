461068f761bf51f1686a2b7bec944e4e78dd9ee6
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 15:39:26 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,11 @@ echo "Starting chisel"
 chisel client --auth ubuntu:ubuntu https://85ptc4-5000.sse.codesandbox.io 1080:socks &
 sleep 10
 echo "Restarting redsocks and redirecting traffic via iptables"
-redsocks -h
-cat /etc/redsocks.conf
-redsocks -c /etc/redsocks.conf
+redsocks -v -c /etc/redsocks.conf
 iptables -t nat -N REDSOCKS
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
 iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
+sleep 10
 echo "curl with socks"
 curl -x socks5://127.0.0.1:1080 ifconfig.me
 echo "curl without socks"
