fe6d5860ca61ab630055efbad3d1dcb2d3271cc5
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 18:14:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var request = require('request');
+var http = require('http');
 
-request({
-	url: 'https://ipecho.net/plain',
-	headers: {
-		'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-	}
-}, function(error, response, body) {
-	if (!error && response.statusCode == 200) {
-		console.log(body);
-	}
-});
+http.createServer(function (req, res) {
+	if (req.url === '/') {
+		res.write('Hello World!');
+		res.end();
+	} else if (req.url === '/test') {
+		request({
+			url: 'https://ipecho.net/plain',
+			headers: {
+				'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+			}
+		}, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				res.write(body);
+				res.end();
+			} else {
+				res.end();
+			}
+		});
+	} 
+}).listen(3000);
