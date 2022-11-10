c76340a76f6f7b4e839e79fa5286235f87cb4771
chouuohc87 <chouuohc87@yahoo.com>
2022-11-10 12:36:27 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,98 +1,74 @@
-const fs = require("fs");
-var http = require("http");
-const superagent = require("superagent");
-const logNetworkTime = require("superagent-node-http-timings");
-const servers = {
-  nl: ["185.52.0.68"],
-  us: [
-    "23.226.231.112",
-    "168.235.78.99",
-    "192.210.229.206",
-    "168.235.81.120",
-    "192.73.235.56"
-  ]
-};
-function downloadSpeed(
-  country = "us",
-  server = servers[country][
-    Math.floor(Math.random() * servers[country].length)
-  ],
-  uploadSpeed = false
-) {
-  return new Promise(async function (resolve) {
-    try {
-      if (!uploadSpeed) {
-        console.log(`Using ${server}!`);
-      }
-      let response = await superagent
-        .get(`http://${server}/100mb.test`)
-        .use(
-          logNetworkTime(function (err, result) {
-            if (!err && result.status === 200) {
-              let speed = (100 / result.timings.total) * 1000;
-              console.log("downloadSpeed:", speed);
-              resolve(speed);
-            }
-          })
-        )
-        .responseType("arraybuffer");
-      try {
-        let stats = await fs.promises.stat("100mb.test");
-        if (stats.size !== 104857600) {
-          await fs.promises.writeFile("100mb.test", response.body);
-        }
-      } catch (err) {
-        await fs.promises.writeFile("100mb.test", response.body);
-      }
-    } catch (err) {
-      resolve(downloadSpeed(country, server));
-    }
-  });
-}
-function uploadSpeed(
-  country = "us",
-  server = servers[country][Math.floor(Math.random() * servers[country].length)]
-) {
-  return new Promise(async function (resolve) {
-    try {
-      console.log(`Using ${server}!`);
-      try {
-        let stats = await fs.promises.stat("100mb.test");
-        if (stats.size !== 104857600) {
-          await downloadSpeed(country, server, true);
-        }
-      } catch (err) {
-        await downloadSpeed(country, server, true);
-      }
-      await superagent
-        .post(`http://${server}/webtests/ul.php`)
-        .attach("file", "./100mb.test")
-        .use(
-          logNetworkTime(function (err, result) {
-            if (!err && result.status === 200) {
-              let speed = (100 / result.timings.total) * 1000;
-              console.log("uploadSpeed:", speed);
-              resolve(speed);
-            }
-          })
-        );
-    } catch (err) {
-      resolve(uploadSpeed(country, server));
-    }
-  });
-}
-http
-  .createServer(async function (req, res) {
-    if (req.url === "/") {
-      res.end("Hello World!");
-    } else if (req.url === "/downloadSpeed") {
-      let speed = await downloadSpeed("nl");
-      res.end(`downloadSpeed: ${speed}`);
-    } else if (req.url === "/uploadSpeed") {
-      let speed = await uploadSpeed("nl");
-      res.end(`uploadSpeed: ${speed}`);
-    } else {
-      res.end();
-    }
-  })
-  .listen(3000);
+const cors = require("cors");
+const compression = require("compression");
+const express = require("express");
+const http = require("http");
+const pty = require("node-pty");
+const WebSocket = require("ws");
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.WebSocketServer({
+	noServer: true,
+	path: "/terminal"
+});
+server.on("upgrade", function(request, socket, head) {
+	wss.handleUpgrade(request, socket, head, function(ws) {
+		wss.emit("connection", ws, request);
+	});
+});
+wss.on("connection", function(ws) {
+	const term = pty.spawn("bash", [], {
+		name: "xterm-color",
+		cols: 30,
+		rows: 80,
+		cwd: process.env.HOME,
+		env: process.env
+	});
+	console.log(`Created terminal with PID: ${ term.pid }`);
+	term.on("data", function(data) {
+		if (ws.readyState === ws.OPEN) {
+			ws.send(JSON.stringify({
+				type: "data",
+				payload: data
+			}));
+		}
+	});
+	term.on("exit", function() {
+		if (ws.readyState === ws.OPEN) {
+			ws.send(JSON.stringify({
+				type: "exit"
+			}));
+			ws.close();
+		}
+	});
+	ws.on("message", function(message) {
+		try {
+			var data = JSON.parse(message);
+			if (data.type === "data") {
+				term.write(data.payload);
+			} else if (data.type === "resize") {
+				const cols = data.payload.cols || 80;
+				const rows = data.payload.rows || 30;
+				term.resize(cols, rows);
+				console.log(`Resized terminal ${ term.pid } to ${ cols } cols and ${ rows } rows`);
+			}
+		} catch (err) {
+			console.log("ws.onmessage", err);
+		}
+	});
+	ws.on("close", function() {
+		term.kill();
+		console.log(`Closed terminal with PID: ${ term.pid }`);
+	});
+});
+app.use(compression());
+app.use(cors());
+app.use("/downloader", express.static("/tmp/AriaNg"));
+app.get("/", function(_, res) {
+  res.send("Hello world!");
+});
+app.get("/terminal", function(_, res) {
+	res.send(`<!DOCTYPE html> <html> <head> <title>Terminal</title> <link href="https://cdn.jsdelivr.net/npm/xterm/css/xterm.css" rel="stylesheet"> <style> html, .terminal { height: 100%; } body, #terminal { height: 100%; margin: 0; } </style> <script src="https://cdn.jsdelivr.net/npm/xterm/lib/xterm.min.js"></script> <script src="https://cdn.jsdelivr.net/npm/xterm-addon-fit/lib/xterm-addon-fit.min.js"></script> <script src="https://cdn.jsdelivr.net/npm/xterm-addon-webgl/lib/xterm-addon-webgl.min.js"></script> </head> <body> <div id="terminal"></div> <script type="text/javascript"> (function connect() { if ("WebSocket" in window) { const el = document.getElementById("terminal"); el.innerHTML = ""; const ws = new WebSocket("wss://" + window.location.host + "/terminal"); const terminal = new Terminal({ allowProposedApi: true, scrollback: 1000, tabStopWidth: 4, fontFamily: "Menlo, Consolas, Liberation Mono, Monaco, Lucida Console, monospace" }); terminal.open(el); terminal.focus(); const fitAddon = new FitAddon.FitAddon(); const webglAddon = new WebglAddon.WebglAddon(); terminal.loadAddon(webglAddon); terminal.loadAddon(fitAddon); terminal.onResize(function(size) { ws.send(JSON.stringify({ "type": "resize", "payload": size })); }); terminal.onData(function(data) { ws.send(JSON.stringify({ "type": "data", "payload": data })); }); ws.onopen = function() { fitAddon.fit(); }; ws.onmessage = function(e) { try { var data = JSON.parse(e.data); if (data.type === "data") { terminal.write(data.payload); } else if (data.type === "exit") { terminal.writeln("Terminal disconnected!"); } } catch (err) { console.log("ws.onmessage", err); } }; ws.onclose = function() { terminal.writeln("Terminal reconnecting..."); setTimeout(connect, 2000); }; window.onresize = function() { fitAddon.fit(); }; window.onbeforeunload = function() { if (ws.readyState === ws.OPEN) { ws.close(); } }; } else { alert("WebSocket is NOT supported by your browser!"); } })(); </script> </body> </html>`);
+});
+server.listen(3000, function() {
+	console.log("Running at Port 3000");
+});
