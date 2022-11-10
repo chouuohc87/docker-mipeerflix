eaa29d44b2c0bbe1f92690f98d8172dbcb7101b4
chouuohc87 <chouuohc87@yahoo.com>
2022-11-09 18:05:01 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo $CHISEL_CLIENT_HOST
 iptables -t nat -N REDSOCKS
 
 # Ignore Chisel traffic
-iptables -t nat -A REDSOCKS -d $CHISEL_CLIENT_HOST -j RETURN
+iptables -t nat -A REDSOCKS -d 85ptc4-5000.sse.codesandbox.io -j RETURN
 
 # Ignore LANs and some other reserved addresses.
 iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
