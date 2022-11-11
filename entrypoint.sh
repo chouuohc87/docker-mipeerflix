6767ec639b03b8809cce9a6a6556c1f4520ba4f9
chouuohc87 <chouuohc87@yahoo.com>
2022-11-11 17:47:47 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,11 @@ iptables -t nat -N REDSOCKS
 
 # Ignore Chisel traffic
 # iptables -t nat -A REDSOCKS -d 85ptc4-5000.sse.codesandbox.io -j RETURN
-iptables -t nat -A REDSOCKS -d wiltedprestigiousmodule0.chouuohc87.repl.co -j RETURN
+# iptables -t nat -A REDSOCKS -d wiltedprestigiousmodule0.chouuohc87.repl.co -j RETURN
+iptables -t nat -A REDSOCKS -d square-eminent-warbler.glitch.me -j RETURN
 
-iptables -t nat -A REDSOCKS -p tcp --dport 6800 -j RETURN
-iptables -t nat -A REDSOCKS -p tcp --sport 6800 -j RETURN
+# iptables -t nat -A REDSOCKS -p tcp --dport 6800 -j RETURN
+# iptables -t nat -A REDSOCKS -p tcp --sport 6800 -j RETURN
 
 # Ignore LANs and some other reserved addresses.
 iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
